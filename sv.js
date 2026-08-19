de0b806d09ed0f6d56aad87bb63a35e9343c1aa2
Flutzy-cloud <olenaesclave@gmail.com>
2026-08-19 10:35:28 -0500

    Add files via upload

---
+++ sv.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+app.use(express.json());
+
+// Se utiliza la variable de entorno DATABASE_URL
+// Si estás probando localmente, puedes usar la cadena por defecto
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_lmaijOoFZk94@ep-morning-pond-ax97krt9-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require',
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get('/api/characters', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM characters');
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Error SQL:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get('/api/characters/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query('SELECT * FROM characters WHERE id = $1', [id]);
+    if (result.rows.length === 0) return res.status(404).send('No encontrado');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error("Error SQL:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Render o cualquier hosting asignará automáticamente el puerto a través de process.env.PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor activo en el puerto ${PORT}`));
